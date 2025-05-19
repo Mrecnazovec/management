5013277ea6da32ab1e3eeeb2232a3ec146d3ab98
Ислом Юлдашев <islomuldashev200512@gmail.com>
2025-05-19 23:55:11 +0500

    remote access

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,7 +3,7 @@ import { Container } from '@/components/ui/Container'
 export default function Home() {
 	return (
 		<div className='text-red-500'>
-			<Container>Homepage</Container>
+			<Container>Новая версия</Container>
 		</div>
 	)
 }
