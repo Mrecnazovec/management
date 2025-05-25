5cd905916c2c2e20e3bd721ad14f1d35fd8e8afd
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-05-25 17:18:11 +0500

    add margin

--- src/components/ui/DataTag.tsx
+++ src/components/ui/DataTag.tsx
@@ -1,6 +1,6 @@
 export function DataTag() {
 	return (
-		<div className='flex justify-end'>
+		<div className='flex justify-end mb-14'>
 			<div className='bg-[#F5F8FD] border-l-3 border-l-black px-9'>
 				<p>25 мая 2025</p>
 			</div>
