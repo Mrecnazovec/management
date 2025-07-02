be94bc1d144d010369cf007af238fbdbf58f6c72
Aleksandr Salnikov <TaynbIy@gmail.com>
2025-07-02 09:03:45 +0500

    new version of svg files in university page

--- src/app/(root)/university/AboutList.tsx
+++ src/app/(root)/university/AboutList.tsx
@@ -1,11 +1,12 @@
-import { PATH_URL } from "@/config/url.config";
-import Image from "next/image";
+import { PATH_URL } from '@/config/url.config'
+import Image from 'next/image'
+import { ListSvg } from './svg/ListSvg'
 
 export function AboutList() {
 	return (
-		<ul className="mb-14">
+		<ul className='mb-14'>
 			<li className='flex justify-between items-center md:gap-8 mb-8 md:flex-row flex-col gap-4'>
-				<Image src={PATH_URL.svg('one.svg')} alt='Первая особенность' width={72} height={72}></Image>
+				<ListSvg variant='one' startColor='bg-main' endColor='bg-secondary' />
 				<p className='text-base'>
 					Отличительной чертой программы «Бакалавр менеджмента» является широта подготовки. Каждый студент программы должен освоить основы наук об
 					управлении: введение в менеджмент, экономика, стратегический менеджмент, операционный менеджмент, управление персоналом, правовая среда
@@ -13,28 +14,28 @@ export function AboutList() {
 				</p>
 			</li>
 			<li className='flex justify-between items-center gap-8 mb-8 max-md:flex-col'>
-				<Image className='md:order-2' src={PATH_URL.svg('two.svg')} alt='Вторая особенность' width={72} height={72}></Image>
+				<ListSvg variant='two' startColor={'bg-main'} endColor={'bg-secondary'} className='md:order-2' />
 				<p className='text-base md:text-end'>
 					Современный менеджер обязан владеть культурой работы с данными — для этого читаются курсы математики, теории вероятностей, статистики,
 					анализа данных, эконометрики.
 				</p>
 			</li>
 			<li className='flex justify-between items-center md:gap-8 mb-8 md:flex-row flex-col gap-4'>
-				<Image src={PATH_URL.svg('three.svg')} alt='Первая особенность' width={72} height={72}></Image>
+				<ListSvg variant='three' startColor={'bg-main'} endColor={'bg-secondary'} />
 				<p className='text-base'>
 					Для ориентации в современной динамичной экономике студентам программы предлагаются курсы по информационным технологиям, цифровой
 					бизнес-среде, архитектуре цифровых предприятия и по цифровым трансформациям отраслей экономики.
 				</p>
 			</li>
 			<li className='flex justify-between items-center gap-8 mb-8 max-md:flex-col'>
-				<Image className='md:order-2' src={PATH_URL.svg('four.svg')} alt='Вторая особенность' width={72} height={72}></Image>
+				<ListSvg variant='four' startColor={'bg-main'} endColor={'bg-secondary'} className='md:order-2' />
 				<p className='text-base md:text-end'>
 					Отдельное внимание на программе «Бакалавр менеджмента» уделяется развитию социальных навыков студентов. Каждый студент слушает обязательные
 					курсы социальной психологии, культуре речи и деловым коммуникациям.
 				</p>
 			</li>
 			<li className='flex justify-between items-center md:gap-8 mb-8 md:flex-row flex-col gap-4'>
-				<Image src={PATH_URL.svg('five.svg')} alt='Первая особенность' width={72} height={72}></Image>
+				<ListSvg variant='five' startColor={'bg-main'} endColor={'bg-secondary'} />
 				<p className='text-base'>
 					На третьем году обучения каждый студент может выбрать дисциплины из нескольких гибких траекторий: Цифровой Маркетинг; Цифровые Таланты;
 					Цифровые Финансы; Цифровые стратегии.
